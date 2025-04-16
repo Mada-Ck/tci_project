bbf4dcd22ca31ac528a594babe250cf260c28c9a
Mada-Ck <kaluamadlitso@yahoo.com>
2025-04-16 06:53:07 +0200

    Compliant with project idx

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,23 +1,44 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.python311
-    pkgs.postgresql
-    pkgs.nodejs
+    pkgs.python3
   ];
-
-  shellHook = ''
-    export PYTHONPATH=$PWD
-    export PGDATA=$PWD/postgres_data
-    export PGHOST=localhost
-    export PGUSER=postgres
-    export PGPASSWORD=postgres
-
-    if [ ! -d $PGDATA ]; then
-      initdb --no-locale --encoding=UTF8
-    fi
-
-    pg_ctl start
-  '';
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-python.python"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        create-venv = ''
+          python -m venv .venv
+          source .venv/bin/activate
+          pip install -r mysite/requirements.txt
+        '';
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = ["README.md" "mysite/mysite/urls.py"];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["./devserver.sh"];
+          env = {
+            PORT = "$PORT";
+          };
+          manager = "web";
+        };
+      };
+    };
+  };
 }
